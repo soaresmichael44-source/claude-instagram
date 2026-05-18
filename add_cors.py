2035524726ec27f9e561063d869ad02d351755ec
Michael Soares <michaelsoares@MacBook-Air-de-Michael.local>
2026-05-18 12:13:47 -0300

    refactor: migrar para Flask com CORS completo

---
+++ add_cors.py
@@ -0,0 +1,34 @@
+with open("servidor.py", "r") as f:
+    content = f.read()
+
+old = '''    def _json(self, code, data):
+        body = json.dumps(data, ensure_ascii=False, indent=2).encode()
+        self.send_response(code)
+        self.send_header("Content-Type", "application/json; charset=utf-8")
+        self.send_header("Content-Length", len(body))
+        self.end_headers()
+        self.wfile.write(body)'''
+
+new = '''    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+
+    def _json(self, code, data):
+        body = json.dumps(data, ensure_ascii=False, indent=2).encode()
+        self.send_response(code)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Content-Type", "application/json; charset=utf-8")
+        self.send_header("Content-Length", len(body))
+        self.end_headers()
+        self.wfile.write(body)'''
+
+if old in content:
+    content = content.replace(old, new)
+    with open("servidor.py", "w") as f:
+        f.write(content)
+    print("OK - CORS adicionado")
+else:
+    print("ERRO - trecho não encontrado")
